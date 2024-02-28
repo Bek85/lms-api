d038b2064a8773f8eb42d62ddc065a62a2e6b96f
Bekzod Akbarov <stx.webdev@gmail.com>
2024-02-28 18:26:49 +0500

    keep empty folders and create Admin model

---
+++ model/Staff/Admin.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const adminSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: 'admin',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Admin = mongoose.model('Admin', adminSchema);
+
+modules.exports = Admin;
